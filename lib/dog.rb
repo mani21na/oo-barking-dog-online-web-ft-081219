7f657860c74189d778b84a2c6970e0d341b4c68e
Hanna Lee <mani21na@gmail.com>
2019-08-19 20:23:35 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+class Dog
+  def name=(dog_name)
+    dog_name = @this_is_dogs_name
+  end 
+  
+  def name
+    @this_is_dogs_name
+  end
+  
+  def bark
+    puts "Woof!"
+  end
+end 
+
+fido = Dog.new
+fido.name = "Fido"
+fido.name
+fido.bark
